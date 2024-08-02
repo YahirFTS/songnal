b4ef419a31f37695e24304d7ff219e413de3b00c
Shutten <yahirftsg@gmail.com>
2024-08-01 23:52:29 -0500

    this is the card that pops once you are in EntryScreen. at this moment, you cant click to write about the suggestions that the app gives to you, but you can click to the reload button to see more suggestions. still have work to do

---
+++ components/screens/MomentCardScreen.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+const MomentCardScreen = ({ suggestion, onRefresh }) => {
+  return (
+    <View style={styles.card}>
+      <Text style={styles.suggestionText}>{suggestion}</Text>
+      <TouchableOpacity onPress={onRefresh}>
+        <Text style={styles.refreshText}>⟲</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: '#444',
+    padding: 15,
+    borderRadius: 10,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginVertical: 10,
+    maxWidth: 300, // Establecemos un tamaño máximo
+    minHeight: 50,
+    right: 20,
+  },
+  suggestionText: {
+    color: '#fff',
+    fontSize: 16,
+    flex:1,
+  },
+  refreshText: {
+    color: '#fff',
+    fontSize: 36,
+    
+    
+  },
+});
+
+export default MomentCardScreen;
